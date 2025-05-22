144a526dc501fb0e5b83cbffa84d246e50774b69
trollgun <guillaume.fontaine.ab@gmail.com>
2025-05-22 16:53:13 +0200

    add conversion for the numbers 2 and 3 to Roman numerals 'II' and 'III'

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -6,12 +6,12 @@ public class RomanNumerals {
     private static final Map<Integer, String> ROMAN_MAP = Map.of(
             10, "X",
             5, "V",
-            1, "I",
-            2, "II",
-            3, "III"
+            1, "I"
     );
 
     public static String convert(int i) {
+        if (i == 2) return "II";
+        if (i == 3) return "III";
         return ROMAN_MAP.getOrDefault(i, "I");
     }
 }
