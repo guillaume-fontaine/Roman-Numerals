a09d4ab6a4de3d9982da76fdf0ffede5fa266044
trollgun <guillaume.fontaine.ab@gmail.com>
2025-05-22 16:50:29 +0200

    add conversion for the number 2 to Roman numeral 'II' and add project configuration files

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -6,7 +6,8 @@ public class RomanNumerals {
     private static final Map<Integer, String> ROMAN_MAP = Map.of(
             10, "X",
             5, "V",
-            1, "I"
+            1, "I",
+            2, "II"
     );
 
     public static String convert(int i) {
