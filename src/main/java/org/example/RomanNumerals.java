4eac556416c32a57c59776142ee3500350c39048
trollgun <guillaume.fontaine.ab@gmail.com>
2025-05-22 16:42:43 +0200

    add conversion for the number 5 to Roman numeral 'V'

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -5,6 +5,8 @@ public class RomanNumerals {
     public static String convert(int i) {
         if (i == 10)
             return "X";
+        if (i == 5)
+            return "V";
         return "I";
     }
 }
